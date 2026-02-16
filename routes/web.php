804d542a115ac5a0e9a21e3b08102269511609aa
Camilo Rojas <crojaslfc@gmail.com>
2026-02-16 18:27:57 -0500

    Creación de rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
-<?php
+            <?php
 
-use Illuminate\Support\Facades\Route;
+            use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+            Route::get('/', function () {
+                return view('welcome');
+            });
+
+            Route::get('/perfil', function () {
+                return view('perfil');
+            });
+
+            Route::get('/perfil/intereses', function () {
+                return view('intereses');
+            });
+
+            Route::get('/perfil/habilidades', function () {
+                return view('habilidades');
+            });
+
+
+            Route::get('/perfil/metas', function () {
+                return view('metas');
+            });
